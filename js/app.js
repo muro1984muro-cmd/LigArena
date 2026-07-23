7b1c2aef09d25fe1052ce6f6fb3e2c23f329340e
Hamza Salman <muro.1984.muro@gmail.com>
2026-07-23 21:07:04 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,82 +1,25 @@
 const API_KEY = "123";
 
-const LIG = "Turkish Super Lig";
-
-
 async function maclariGetir(){
 
 try{
 
 const cevap = await fetch(
-`https://www.thesportsdb.com/api/v1/json/${API_KEY}/eventsnextleague.php?id=4339`
+"https://www.thesportsdb.com/api/v1/json/123/eventsnextleague.php?id=4339"
 );
-
 
 const veri = await cevap.json();
 
-
 console.log(veri);
-
-
-let alan = document.querySelector("#maclar");
-
-
-let html = "";
-
-
-if(veri.events){
-
-veri.events.slice(0,5).forEach(mac=>{
-
-
-html += `
-
-<div class="featured-match">
-
-<h2>⚽ ${mac.strHomeTeam} - ${mac.strAwayTeam}</h2>
-
-<div class="score">
-
-<h1>
-${mac.dateEvent}
-</h1>
-
-<p>
-${mac.strTime || ""}
-</p>
-
-</div>
-
-</div>
-
-`;
-
-});
-
-
-alan.innerHTML += html;
-
-
-}else{
-
-
-alan.innerHTML += `
-<p>Maç bulunamadı.</p>
-`;
-
-}
-
 
 }
 
 catch(hata){
 
-console.log("Hata:",hata);
+console.log("Hata:", hata);
 
 }
 
-
 }
-
 
 maclariGetir();
